8c2adb3da85685b357409fd2534416d160c43e26
Agustin Quiroga <agusquiroga648@gmail.com>
2022-09-15 00:07:23 -0300

    segunda entrega lista

---
+++ entrega.js
@@ -0,0 +1,15 @@
+
+const nombre = "Agustin";
+const edad = 20;
+const desarrollador = true;
+const nacimiento = new Date (2001,5, 25);
+//agrego pelicula porque no leo libros
+const pelicula = {
+    titulo: "Avengers: End Game",
+    autor: "Stan Lee",
+    fecha: new Date (2019,3,26),
+}
+
+
+const lista = [nombre,edad,desarrollador,nacimiento, pelicula]
+console.log(lista)
